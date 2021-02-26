61590f8c469e2885cd51ff0691fcaef3dc2f6594
Katerina <katerina_mutafova@abv.bg>
2021-02-26 14:02:17 +0200

    solved

---
+++ 2. Classes and instances/classes_lab_ex2_vet.py
@@ -0,0 +1,30 @@
+class Vet:
+    animals = []
+    space = 5
+
+
+    def __init__(self, name: str):
+        self.name = name
+        self.animals = []
+
+    def register_animal(self, animal_name: str):
+        if Vet.space > 0:
+            Vet.space -= 1
+            self.animals.append(animal_name)
+            Vet.animals.append(animal_name)
+            return f"{animal_name} registered in the clinic"
+        return "Not enough space"
+
+    def unregister_animal(self, animal_name: str):
+        if animal_name in self.animals:
+            self.animals.remove(animal_name)
+            Vet.animals.remove(animal_name)
+            Vet.space += 1
+            return f"{animal_name} unregistered successfully"
+        return f"{animal_name} not in the clinic"
+
+
+    def info(self):
+        return f"{self.name} has {len(self.animals)} animals. {Vet.space} space left in clinic"
+
+

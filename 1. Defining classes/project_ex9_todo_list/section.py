ed7a8c45740064c3961401a5ef53f1007d7c2e32
Katerina <katerina_mutafova@abv.bg>
2021-02-26 12:55:54 +0200

    ex9 solved

---
+++ 1. Defining classes/project_ex9_todo_list/section.py
@@ -0,0 +1,51 @@
+from project_ex9_todo_list.task import Task
+
+
+class Section:
+    def __init__(self, name):
+        self.name = name
+        self.tasks = []
+
+    def add_task(self, new_task: Task):
+        if new_task in self.tasks:
+            return f"Task is already in the section {self.name}"
+        self.tasks.append(new_task)
+        return f"Task {new_task.details()} is added to the section"
+
+    def complete_task(self, task_name: str):
+        filtered_tasks = [t for t in self.tasks if t.name == task_name]
+        if not filtered_tasks:
+            return f"Could not find task with the name {task_name}"
+        the_task = filtered_tasks[0]
+        the_task.completed = True
+        return f"Completed task {the_task.name}"
+
+    def clean_section(self):
+        completed_tasks = [t for t in self.tasks if t.completed == True]
+        uncompleted_tasks = [t for t in self.tasks if t.completed == False]
+        self.tasks = uncompleted_tasks
+        return f"Cleared {len(completed_tasks)} tasks."
+
+    def view_section(self):
+        result = f"Section {self.name}:\n"
+        for t in self.tasks:
+            result += f"{t.details()}\n"
+        return result
+
+
+
+
+task = Task("Make bed", "27/05/2020")
+print(task.change_name("Go to University"))
+print(task.change_due_date("28.05.2020"))
+task.add_comment("Don't forget laptop")
+print(task.edit_comment(0, "Don't forget laptop and notebook"))
+print(task.details())
+section = Section("Daily tasks")
+print(section.add_task(task))
+second_task = Task("Make bed", "27/05/2020")
+section.add_task(second_task)
+print(section.clean_section())
+print(section.view_section())
+
+
